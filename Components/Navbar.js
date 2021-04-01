4a2d01ef9237052988aa84189cbbda752ee39499
Guido <guido.usnayo.v@gmail.com>
2021-03-31 22:12:28 -0400

    screen fix add new vh

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -3,7 +3,7 @@ const Navbar = () => {
   return (
     <div>
       {/*<!-- This example requires Tailwind CSS v2.0+ -->*/}
-<nav className="bg-gray-800">
+<nav className="bg-gray-800 fixed w-screen">
   <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <div className="flex items-center justify-between h-16">
       <div className="flex items-center">
